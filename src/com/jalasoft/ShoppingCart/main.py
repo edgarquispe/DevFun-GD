ee035abe75a593b07a566acd19d6084a09d6bf65
edgarquispe <edgartron32@gmail.com>
2019-03-07 17:59:10 -0800

    we worked on windows for product menu

--- src/com/jalasoft/ShoppingCart/main.py
+++ src/com/jalasoft/ShoppingCart/main.py
@@ -1,8 +1,13 @@
-from src.com.jalasoft.ShoppingCart.controller.controller import Controller
-from src.com.jalasoft.ShoppingCart.model.model import Model
-from src.com.jalasoft.ShoppingCart.view.view import View
+import sys
+from sys import argv
 
-if __name__ == "__main__":
+from src.com.jalasoft.ShoppingCart.controller.Controller import Controller
+from src.com.jalasoft.ShoppingCart.view.view import View
+from src.com.jalasoft.ShoppingCart.model.model import Model
+from PyQt5.QtWidgets import QApplication
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
     view = View()
     model = Model()
     controller = Controller(view, model)
+    sys.exit(app.exec_())
--- src/com/jalasoft/ShoppingCart/view/view.py
+++ src/com/jalasoft/ShoppingCart/view/view.py
@@ -1,6 +1,44 @@
-class View:
+from PyQt5.QtWidgets import QMainWindow, QMenu, QWidget, QLineEdit, QLabel, QFormLayout
+from self import self
+
+
+class View(QMainWindow):
+
     def __init__(self):
-        print("view")
+        super().__init__()
+        # print('view')
 
     def initUI(self):
-        print("initUI")
+        self.setWindowTitle("TitleTest")
+        self.__initComponent()
+        self.show()
+        # print('initUI')
+
+    def __initComponent(self):
+        menuBar = self.menuBar()
+        product = menuBar.addMenu("Product")
+        insert = QMenu("Insert", self)
+        product.addMenu(insert)
+        self.setCentralWidget(self.__getProductView())
+    def __getProductView(self):
+        proView = ProductView()
+        return proView
+
+# class ProductView(QWidget):
+#     def __init__(self, parent=None):
+#         super(ProductView,self).__init__(parent)
+#         self._initUI()
+#     def _initUI(self):
+#         form = QFormLayout()
+#         form.addRow(QLabel("name:"), QLineEdit())
+
+class ProductView(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        form = QFormLayout()
+        form.addRow(QLabel('name'), QLineEdit())
+        self.setLayout(form)
