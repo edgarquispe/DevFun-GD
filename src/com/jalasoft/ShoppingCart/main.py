a396373e2a5ddd0bdc7776b57e3f256f245b2763
Magali <magali.fuentes@jalasoft.com>
2019-03-08 11:37:52 -0400

    Here we have the last changes of ProductView class, including main and view classes.

--- src/com/jalasoft/ShoppingCart/main.py
+++ src/com/jalasoft/ShoppingCart/main.py
@@ -1,8 +1,14 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication
+
 from src.com.jalasoft.ShoppingCart.controller.controller import Controller
 from src.com.jalasoft.ShoppingCart.model.model import Model
 from src.com.jalasoft.ShoppingCart.view.view import View
 
 if __name__ == "__main__":
+    app = QApplication(sys.argv)
     view = View()
     model = Model()
     controller = Controller(view, model)
+    sys.exit(app.exec_())
